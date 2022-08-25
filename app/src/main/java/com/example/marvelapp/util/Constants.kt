9c8b74eb5e57cf9f29b5ae68706551166a843cfc
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-08-25 18:28:03 -0300

    build(DI): Concluindo implementação do Hilt

---
+++ app/src/main/java/com/example/marvelapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.marvelapp.util
+
+object Constants {
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+
+    const val PUBLIC_KEY = "e9546170a15986891589093d20f3ca47"
+    const val PRIVATE_KEY = "09b52097bdb769e27e29b0676c79d40eed92235b"
+}
