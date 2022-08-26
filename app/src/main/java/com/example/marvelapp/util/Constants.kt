1d64ed9a8523041cc4f4f9e4e466d0b85b15366a
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2022-08-26 20:17:02 -0300

    build(ViewModel): Implementando ViewModel(Deu um erro)

--- app/src/main/java/com/example/marvelapp/util/Constants.kt
+++ app/src/main/java/com/example/marvelapp/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.marvelapp.util
 
 object Constants {
-    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
 
     const val PUBLIC_KEY = "e9546170a15986891589093d20f3ca47"
     const val PRIVATE_KEY = "09b52097bdb769e27e29b0676c79d40eed92235b"
